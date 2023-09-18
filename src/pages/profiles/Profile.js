c7733604feb4899ff731a364078d4695c1d3255c
Edward Shanahan <edwardshanahan357@gmail.com>
2023-09-18 19:39:27 +0000

    Add functionality to edit profile page

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -7,6 +7,7 @@ import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
 import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
+
 const Profile = (props) => {
   const { profile, mobile, imageSize = 55 } = props;
   const { id, following_id, image, owner } = profile;
